cf09847a115b5b1eb3eaaf544d6461e1b1c8879a
CreateSource <abnormalhare822@gmail.com>
2022-02-19 13:53:46 -0500

    oi

--- boot.py
+++ boot.py
@@ -1,54 +1,16 @@
 from saveloader import detectSave, loadSystemSave
 from rich import print as rprint
-from rich.console import Console
 from clear import clear
 from checkbadge import calculateBadge
 from time import sleep
 import sys
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-from player import beginMenu
-from mod import *
-import re
-
-width = 120
-console = Console(width=width)
-message = ""
-=======
-=======
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
-=======
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
 from mod import systemList, proList
 from player import startGame, beginMenu, pauseBeginMenu
 
 # compressed code helps make game much more expandable/moddable
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
-=======
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
-=======
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
 
 # systems
 sys.path.insert(0, './oses/')
-
-def fillBg():
-    global message
-    message2 = message
-    message3 = ""
-    x = 0
-    for i in message:
-        if i == "[":
-            x += 1
-        elif i == "]" and x:
-            x -= 1
-        elif x == 0:
-            message3 += i
-    message2 += " " * (width - len(message3))
-    message = message2
 
 def launch(systemOS, systemlevel, systembadge, systempro):
     clear()
@@ -76,32 +38,8 @@ def boot():
     detectSave()
 
     global currentSystem
-    global width
-    global message
 
     clear()
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-    message = 'MiniChipOS ver. 0.59 - [bright_yellow]Codename Cookie Monster[/bright_yellow]'
-    style = "default on dark_blue"
-    fillBg()
-    console.print(message, style=style)
-    for i in range(len(pbList)):
-        stri = pbList[i]
-        stri1 = pbList[i - 1]
-        save = loadSystemSave(stri)
-        if save == False:
-            message = f'[red]{i + 1}. {stri} - Get to level {pbProList[i - 1]} in {stri1} to unlock this![/red]'
-            fillBg()
-            console.print(message, style=style)
-        else:
-            badge = calculateBadge(save, pbProList[i])
-            message = f"[#00bb00]{i + 1}. {stri} {badge}[/#00bb00]"
-            fillBg()
-            console.print(message, style=style)
-=======
     rprint('MiniChipOS ver. 0.56 - [bright_yellow]Codename Nubo[/bright_yellow]')
     print('Ver. 12-30-2021\n\n')
     
@@ -114,35 +52,6 @@ def boot():
         else:
             badge = calculateBadge(save, proList[i])
             print(f"{i + 1}. {stri}", badge)
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
-=======
-    rprint('MiniChipOS ver. 0.56 - [bright_yellow]Codename Nubo[/bright_yellow]')
-    print('Ver. 12-30-2021\n\n')
-    
-    for i in range(len(systemList)):
-        stri = systemList[i]
-        stri1 = systemList[i - 1]
-        save = loadSystemSave(stri)
-        if save == False:
-            rprint(f'[red]{i + 1}. {stri} - Get to level {proList[i - 1]} in {stri1} to unlock this![/red]')
-        else:
-            badge = calculateBadge(save, proList[i])
-            print(f"{i + 1}. {stri}", badge)
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
-=======
-    rprint('MiniChipOS ver. 0.56 - [bright_yellow]Codename Nubo[/bright_yellow]')
-    print('Ver. 12-30-2021\n\n')
-    
-    for i in range(len(systemList)):
-        stri = systemList[i]
-        stri1 = systemList[i - 1]
-        save = loadSystemSave(stri)
-        if save == False:
-            rprint(f'[red]{i + 1}. {stri} - Get to level {proList[i - 1]} in {stri1} to unlock this![/red]')
-        else:
-            badge = calculateBadge(save, proList[i])
-            print(f"{i + 1}. {stri}", badge)
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
 
     choice = input()
     startup(choice)
--- player.py
+++ player.py
@@ -1,49 +1,24 @@
 from clear import clear
-from rich.console import Console
+from rich import print as rprint
 from time import sleep
 import random
 from saveloader import editSystemSave, addSystemSave
 from checkbadge import calculateBadge
 from mod import addtionalFeatures
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-from mod import *
-import os
-=======
 from mod import systemList as OSList
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
-=======
-from mod import systemList as OSList
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
-=======
-from mod import systemList as OSList
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
 cancelPopUp = False
 diffLevel = 1
-message = ""
-style = "default on turquoise4"
-
-width = 120
-console = Console(width=width)
-
-
 
 
 def wait():
-    global message
     clear()
-    message = 'P l e a s e  w a i t . . .\n\n\n'
-    
+    print('P l e a s e  w a i t . . .\n\n\n')
     sleep(3)
 
 # shutdown woohoo
 def shutdown():
-    global message
     wait()
-    message = '[bold orange]It is now safe to close your Command Line Interface.[/bold orange]'
-    
-    console.print(message, style=style)
+    rprint('[bold orange]It is now safe to close your Command Line Interface.[/bold orange]')
     sleep(2)
     quit()
 
@@ -54,14 +29,11 @@ def restart():
 
 # Begin menu normally
 def beginMenu(systemname, systemlevel, systempro):
-    global message
     clear()
     if systemlevel > 1:
-        message = '╔════════════════════════╗\n║   B e g i n  M e n u   ║\n║    1 - Load Game       ║\n║    2 - New Game        ║\n║    3 - Restart         ║\n║    4 - Shutdown        ║\n╚════════════════════════╝'
-        
+        print('╔════════════════════════╗\n║   B e g i n  M e n u   ║\n║    1 - Load Game       ║\n║    2 - New Game        ║\n║    3 - Restart         ║\n║    4 - Shutdown        ║\n╚════════════════════════╝\n')
     else:
-        message = '╔════════════════════════╗\n║   B e g i n  M e n u   ║\n║    1 - New Game        ║\n║    2 - Restart         ║\n║    3 - Shutdown        ║\n╚════════════════════════╝'
-        
+        print('╔════════════════════════╗\n║   B e g i n  M e n u   ║\n║    1 - New Game        ║\n║    2 - Restart         ║\n║    3 - Shutdown        ║\n╚════════════════════════╝\n')
     choice = input()
     if choice == "1":
         if systemlevel > 1:
@@ -92,7 +64,7 @@ def beginMenu(systemname, systemlevel, systempro):
 # Begin menu during gameplay
 def pauseBeginMenu(systemName, systemPro):
     clear()
-    console.print('╔════════════════════════╗\n║   B e g i n  M e n u   ║\n║    1 - Resume          ║\n║    2 - New Game        ║\n║    3 - Restart         ║\n║    4 - Shutdown        ║\n╚════════════════════════╝', style=style)
+    print('╔════════════════════════╗\n║   B e g i n  M e n u   ║\n║    1 - Resume          ║\n║    2 - New Game        ║\n║    3 - Restart         ║\n║    4 - Shutdown        ║\n╚════════════════════════╝\n')
     choice = input()
     if choice == "1":
         return
@@ -109,16 +81,16 @@ def pauseBeginMenu(systemName, systemPro):
 # original code by Setapdede, but i refined it a bit.
 def spawnPopup(startLevel, systemLabel):
     clear()
-    console.print('Level', startLevel)
+    print('Level', startLevel)
     global cancelPopUp
     global popNum
     if systemLevel > 0:
-        console.print('<', systemLabel, '>', style=style)
+        print('<', systemLabel, '>')
     if cancelPopUp == False:
         popNum = random.randint(0, 2)
     popText = ["Annoying Popup", " Can I help?  ", "  Hello!      "]
     popup = popText[popNum]
-    console.print("[bold bright_black]╔════════════════════╗\n║[/bold bright_black] :) ", popup, "[bold bright_black]║\n║[/bold bright_black]        [OK]        [bold bright_black]║\n╚════════════════════╝[/bold bright_black]")
+    rprint("[bold bright_black]╔════════════════════╗\n║[/bold bright_black] :) ", popup, "[bold bright_black]║\n║[/bold bright_black]        [OK]        [bold bright_black]║\n╚════════════════════╝[/bold bright_black]")
     popupinput = input()
     if popupinput.lower() == "ok":
         clear()
@@ -172,36 +144,12 @@ def startGame(systemName, startLevel, proLevel):
         # checks if lives are 0, breaks if true
                 # checks if lives are 0, breaks if true
         if lives <= 0:
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-            console.console.print("[bold bright_blue]You are out of lives. Game over![/bold bright_blue]", style=style)
-            if startLevel == 1:
-                console.console.print('[i]A level has not been taken.[/i]', style=style)
-            else:
-                startLevel -= 1
-                console.console.print('[bold i]-1 Level[/bold i]', style=style)
-=======
-=======
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
             rprint("[bold bright_blue]You are out of lives. Game over![/bold bright_blue]")
             if startLevel == 1:
                 rprint('[i]A level has not been taken.[/i]')
             else:
                 startLevel -= 1
                 rprint('[bold i]-1 Level[/bold i]')
-<<<<<<< HEAD
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
-=======
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
-=======
-            rprint("[bold bright_blue]You are out of lives. Game over![/bold bright_blue]")
-            if startLevel == 1:
-                rprint('[i]A level has not been taken.[/i]')
-            else:
-                startLevel -= 1
-                rprint('[bold i]-1 Level[/bold i]')
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
                 editSystemSave(systemName, startLevel)
             lives = 3
             sleep(3)
@@ -212,9 +160,9 @@ def startGame(systemName, startLevel, proLevel):
             cancelPopUp = False
             spawnPopup(startLevel, systemLabel)
 
-        console.print('Level', startLevel)
+        print('Level', startLevel)
         if systemLevel > 0:
-            console.print('<', systemLabel, '>', style=style)
+            print('<', systemLabel, '>')
 
         # randomly chooses a segment and loads art
         segArt = ["[blue]╔══╗\n║  ║\n║  ║\n╚══╝[/blue]", "[bright_red]╔══╗\n║!!║\n║!!║\n╚══╝[/bright_red]", "[bright_magenta]╔══╗\n║--║\n║--║\n╚══╝[/bright_magenta]", "[bright_yellow]╔══╗\n║~~║\n║~~║\n╚══╝[/bright_yellow]", "[bright_black]╔══╗\n║..║\n║..║\n╚══╝[/bright_black]", "[bright_cyan]╔══╗\n║**║\n║**║\n╚══╝[/bright_cyan]"]
@@ -222,19 +170,7 @@ def startGame(systemName, startLevel, proLevel):
             segEasy = random.randint(0, 11)
             if segEasy <= 3:
                 seg = 0
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-                console.print(segArt[seg], style=style)
-=======
                 rprint(segArt[seg])
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
-=======
-                rprint(segArt[seg])
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
-=======
-                rprint(segArt[seg])
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
             elif segEasy == 3:
                 seg = 1
             elif segEasy == 4:
@@ -245,52 +181,17 @@ def startGame(systemName, startLevel, proLevel):
                 seg = 4
             elif segEasy >= 9:
                 seg = 5
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-            console.print(segArt[seg], style=style)
-=======
             rprint(segArt[seg])
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
-=======
-            rprint(segArt[seg])
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
-=======
-            rprint(segArt[seg])
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
         if diffLevel == 1:
             seg = random.randint(0, 5)
             for i in range(6):
                 if seg == i:
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-                    console.print(segArt[i], style=style)
-=======
                     rprint(segArt[i])
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
-=======
-                    rprint(segArt[i])
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
-=======
-                    rprint(segArt[i])
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
 
         # green segment check
         greenseg = random.randint(0, 100)
         if greenseg == 95:
             seg = 6
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-            console.print("[bright_green]╔══╗\n║$$║\n║$$║\n╚══╝[/bright_green]", style=style)
-
-        # checks if you have 1 life left
-        if lives == 1:
-            console.print("You have [italic bright_red]1 life left[/italic bright_red]. Be careful.", style=style)
-        else:
-            console.print("You have", lives, "lives left.", style=style)
-=======
             rprint("[bright_green]╔══╗\n║$$║\n║$$║\n╚══╝[/bright_green]")
 
         # checks if you have 1 life left
@@ -298,187 +199,143 @@ def startGame(systemName, startLevel, proLevel):
             rprint("You have [italic bright_red]1 life left[/italic bright_red]. Be careful.")
         else:
             print("You have", lives, "lives left.")
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
-=======
-=======
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
-            rprint("[bright_green]╔══╗\n║$$║\n║$$║\n╚══╝[/bright_green]")
-
-        # checks if you have 1 life left
-        if lives == 1:
-            rprint("You have [italic bright_red]1 life left[/italic bright_red]. Be careful.")
-        else:
-            print("You have", lives, "lives left.")
-<<<<<<< HEAD
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
-=======
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
 
         # checks if you have orange segments in your bar
         if progressbar2 > 0:
-            console.print('\nYour bar:', end='', style=style)
+            print('\nYour bar:', end='')
             for segment in bar2:
                 if segment == "Blue":
-                    console.print("[blue][][/blue]", end='', style=style)
+                    rprint("[blue][][/blue]", end='')
                 elif segment == "Orange":
-<<<<<<< HEAD
-                    console.print("[bright_yellow][][/bright_yellow]", end='', style=style)
-            console.print("\nYou have", progressbar, "% with", progressbar2, "% orange in your progressbar.", style=style)
-=======
                     rprint("[bright_yellow][][/bright_yellow]", end='')
             print("\nYou have", progressbar, "% with", progressbar2, "% orange in your progressbar.")
-<<<<<<< HEAD
-<<<<<<< HEAD
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
-=======
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
-=======
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
         else:
-            console.print('\nYour bar:', end='', style=style)
+            print('\nYour bar:', end='')
             for segment in bar2:
                 if segment == "Blue":
-                    console.print("[blue][][/blue]", end='', style=style)
-            console.print("\nYou have", progressbar,"%", "in your progressbar.")
+                    rprint("[blue][][/blue]", end='')
+            print("\nYou have", progressbar,"%", "in your progressbar.")
 
         # catches the currently displayed segment
         catch = input("Type 'c' to catch, any other key to move away, and 'q' to quit.\n")
 
         # calculates which segment you caught and does stuff
-        if catch == "c":
-            if seg == 0:
-                progressbar = progressbar + 5
-                bar2.append("Blue")
-                score = score + 5
-            elif seg == 1:
-                bar = []
-                bar2 = []
-                bardisplay = ""
-                lives -= 1
-                progressbar = 0
-                progressbar2 = 0
-                score -= 10
-            elif seg == 2:
-                if progressbar == 0:
-                    continue
-                if bar2[-1] == "Orange":
-                    progressbar2 -= 5
-                    progressbar -= 5
-                    bar2.pop(-1)
-                    score += 5
-                else:
-                    progressbar -= 5
-                    bar2.pop(-1)
-                    score -= 5
-            elif seg == 3:
-                progressbar += 5
-                progressbar2 += 5
-                bar2.append("Orange")
-            elif seg == 4:
+        if seg == 0 and catch == "c":
+            progressbar = progressbar + 5
+            bar2.append("Blue")
+            score = score + 5
+        elif seg == 1 and catch == "c":
+            bar = []
+            bar2 = []
+            bardisplay = ""
+            lives = lives - 1
+            progressbar = 0
+            progressbar2 = 0
+            score = score - 10
+        elif seg == 2 and catch == "c":
+            if progressbar == 0:
                 continue
-            elif seg == 5:
-                bonus = random.randint(0, 1)
-                if bonus == 0:
-                    progressbar += 10
-                    for i in range(2):
-                        bar2.append("Blue")
-                    score += 10
-                else:
-                    progressbar += 15
-                    for i in range(3):
-                        bar2.append("Blue")
-                    score += 15
-            elif seg == 6:
-                progressbar = 100
-                progressbar2 = 0
-                score += 100
+            if bar2[-1] == "Orange":
+                progressbar2 = progressbar2 - 5
+                progressbar = progressbar - 5
+                bar2.pop(-1)
+                score = score + 5
+            else:
+                progressbar = progressbar - 5
+                bar2.pop(-1)
+                score + score - 5
+        elif seg == 3 and catch == "c":
+            progressbar = progressbar + 5
+            progressbar2 = progressbar2 + 5
+            bar2.append("Orange")
+        elif seg == 4 and catch == "c":
+            continue
+        elif seg == 5 and catch == "c":
+            bonus = random.randint(0, 1)
+            if bonus == 0:
+                progressbar = progressbar + 10
+                bar2.append("Blue")
+                bar2.append("Blue")
+                score = score + 10
+            else:
+                progressbar = progressbar + 15
+                bar2.append("Blue")
+                bar2.append("Blue")
+                bar2.append("Blue")
+                score = score + 15
+        elif seg == 6 and catch == "c":
+            progressbar = 100
+            progressbar2 = 0
+            score = score + 100
 
-        elif catch == "q":
-            console.print('Game Over! Thanks for playing!', style=style)
+        if catch == "q":
+            print('Game Over! Thanks for playing!')
             sleep(3)
             beginMenu(systemName, startLevel, proLevel)
 
-        elif catch == "credits":
+        if catch == "credits":
             clear()
-            console.print('ProgressCLI95 0.2.2 Release', style=style)
-            console.print('Original code (0.1) by Setapdede', style=style)
-            console.print('Improved code (0.2-0.2.2) by BurningInfern0', style=style)
-            console.print('Moddable code (0.3+) by CreateSource/AbnormalHare', style=style)
-            console.print('Made for use with Sparrow Assistant by pivinx1', style=style)
-            console.print('\nPress ENTER to get back to the game.', style=style)
+            print('ProgressCLI95 0.2.2 Release')
+            print('Original code (0.1) by Setapdede')
+            print('Improved code (0.2-0.2.2) by BurningInfern0')
+            print('Moddable code (0.3+) by CreateSource/AbnormalHare')
+            print('Made for use with Sparrow Assistant by pivinx1')
+            print('\nPress ENTER to get back to the game.')
             input()
 
-        elif catch == "beginmenu":
+        if catch == "beginmenu":
             pauseBeginMenu(systemName, proLevel)
 
         # if you have 100% on your progressbar, the game will end.
         if progressbar >= 100:
             if progressbar2 > 0:
-                console.print('Bravo!', style=style)
+                print('Bravo!')
             elif progressbar >= 100 and progressbar2 == 0:
-                console.print('Perfect!', style=style)
+                print('Perfect!')
             elif progressbar > 100:
-                console.print('Outer space!', style=style)
+                print('Outer space!')
             if progressbar == 50 and progressbar2 == 50:
-                console.print ('Yin and yang', style=style)
+                print ('Yin and yang')
             if progressbar == 0 and progressbar2 == 100:
-                console.print ("Nonconformist!")
+                print ("Nonconformist!")
             startLevel += 1
             editSystemSave(systemName, startLevel)
             if startLevel == proLevel:
-                console.print('\nCongratulations! You are the Professional!', style=style)
-                console.print('Pro Label acquired!', style=style)
+                print('\nCongratulations! You are the Professional!')
+                print('Pro Label acquired!')
                 systemLevel = 1
                 systemLabel = "Pro"
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-                console.print(f'{systemName} unlocked...', style=style)
-                for i in range(len(pbList)):
-                    if systemName == pbList[i]:
-=======
                 print(f'{systemName} unlocked...')
                 for i in range(len(OSList)):
                     if systemName == OSList[i]:
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
-=======
-                print(f'{systemName} unlocked...')
-                for i in range(len(OSList)):
-                    if systemName == OSList[i]:
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
-=======
-                print(f'{systemName} unlocked...')
-                for i in range(len(OSList)):
-                    if systemName == OSList[i]:
->>>>>>> parent of 33673a8 (UNFINISHED SAVE)
                         try:
                             addSystemSave(OSList[i + 1])
                         except:
                             addSystemSave(OSList[1])
             elif startLevel == 100:
-                console.print('\nExpert Label acquired!', style=style)
+                print('\nExpert Label acquired!')
                 systemLevel = 2
                 systemLabel = "Expert"
             elif startLevel == 250:
-                console.print('\nMaster Label acquired!', style=style)
+                print('\nMaster Label acquired!')
                 systemLevel = 3
                 systemLabel = "Master"
             elif startLevel == 500:
-                console.print('\nAdept Label acquired!', style=style)
+                print('\nAdept Label acquired!')
                 systemLevel = 4
                 systemLabel = "Adept"
             elif startLevel == 1000:
-                console.print('\nGrand Label acquired!', style=style)
+                print('\nGrand Label acquired!')
+            elif startLevel == 2147483647:
+                print('\nWhat?')
                 systemLevel = 5
                 systemLabel = "Grand"
-            elif startLevel == 2147483647:
-                console.print('\nWhat?', style=style)
             bar = []
             bar2 = []
             bardisplay = ""
             segments = ""
             progressbar = 0
             progressbar2 = 0
-            console.print('\nPress ENTER to play another level.', style=style)
+            print('\nPress ENTER to play another level.')
             input()
         continue
